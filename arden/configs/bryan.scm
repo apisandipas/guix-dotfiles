fd74accc06650dc42f9ef06920ea718ccc4a6dcd
Bryan Paronto <bryan@cablecar.digital>
2022-06-19 18:34:44 -0500

    import fonts module, signed this time

--- arden/configs/bryan.scm
+++ arden/configs/bryan.scm
@@ -33,9 +33,6 @@
      #:gpg-smart-card? #f)
     (feature-virtualization)
     (feature-qutebrowser)
-    (feature-fonts
-    #:font-monospace (font "Iosevka" #:size 11 #:weight 'regular)
-    #:font-packages (list font-iosevka font-fira-mono))
     (feature-firefox
      #:default-browser? #t)
     (feature-kdeconnect))))
